60b2563af164944f1c3fe0db7be90aa3420bcf4e
mohamadj0 <khedmati.mo198@gmail.com>
2022-08-25 14:50:08 +0300

    create derail view

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('<slug:slug>/', views.DetailProductView.as_view(), name='product_detail')
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
-
+from .models import Product
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/home.html')
+        product = Product.objects.filter(available=True)
+        return render(request, 'home/home.html', {'products':product})
+
+class DetailProductView(View):
+    def get(self, request, slug):
+        product = get_object_or_404(Product, slug=slug)
+        return render(request, 'home/detail.html', {'product':product})
