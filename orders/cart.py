c6bc23f0432a783d69371bfc770643a49ce47d6f
mohamadj0 <khedmati.mo198@gmail.com>
2022-09-03 15:21:40 +0300

    context_processors

--- orders/cart.py
+++ orders/cart.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 from home.models import Product
 
 
@@ -39,7 +40,8 @@ class Cart:
     def get_total_price(self):
         return sum(int(item['price']) * item['quantity'] for item in self.cart.values())
         
-
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
 
     def save(self):
         self.session.modified = True
