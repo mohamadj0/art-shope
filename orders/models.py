a39f5a881a323568bb996e848e36067aa3f73f29
mohamadj0 <khedmati.mo198@gmail.com>
2022-09-04 10:47:12 +0300

    create model order

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from accounts.models import User
+from home.models import Product
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    paid = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('paid', '-updated')
+
+    def __str__(self):
+        return f'{self.user} - {str(self.id)}'
+    
+    def get_total_price(self):
+        return sum(item.get_cost() for item in self.item.all())
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='item')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.product} - {self.price}'
+    
+    def get_cost(self):
+        return self.price * self.quantity
